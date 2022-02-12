c0de5e39143b15cc3f7ba1485777763ce4caede1
Dennis Bronnbauer <95525399+Dele246@users.noreply.github.com>
2022-02-12 11:06:16 +0100

    Change for #1 (#2)
    * initial
    
    * initial
    
    * formatted black
    
    * Check Content-Type
    
    * Flake8 ready
    
    * black ready
    
    * black and flake8 ready
    
    * Change Function to receiveData
    
    * Call validateJSON
    
    * Change: Call validateJSON
    
    * Get Response from validateJSON
    
    * Add print statements
    
    * Add String-Cast to return
    
    * Change Return-Parameters
    
    * Finalize validateJSON call

---
+++ lambda_function.py
@@ -0,0 +1,44 @@
+import json
+import boto3
+
+CONTENT_TYPE = "Content-Type"
+CONTENT_TYPE_VAL = "application/json"
+INVOCATION_TYPE = "RequestResponse"
+VAL_FUNC = "arn:aws:lambda:eu-central-1:255547448515:function:validateJSON"
+CON_FUNC = "arn:aws:lambda:eu-central-1:255547448515:function:convertJSONtoCSV"
+
+
+def lambda_handler(event, context):
+    client = boto3.client("lambda")
+    header = event["headers"]
+
+    # Check Content-Type
+    if header[CONTENT_TYPE] != CONTENT_TYPE_VAL:
+        status_txt = "Wrong Content-Type! Use " + CONTENT_TYPE_VAL
+        print(status_txt)
+        return {"statusCode": 400, "body": status_txt}
+
+    # Validate JSON
+    inputParams = {"JSON": event["body"]}
+
+    resValJSON = client.invoke(
+        FunctionName=VAL_FUNC,
+        InvocationType=INVOCATION_TYPE,
+        Payload=json.dumps(inputParams),
+    )
+
+    responseJson = json.load(resValJSON["Payload"])
+    for key, value in responseJson.items():
+        if key == "statusCode":
+            statusCode = value
+        elif key == "statusTxt":
+            statusTxt = value
+
+    print(responseJson)
+    if statusCode != 200:
+        return {"statusCode": statusCode, "body": str(statusTxt)}
+
+    # Convert JSON to CSV
+    # ToDo
+
+    return {"statusCode": statusCode, "body": str(statusTxt)}
