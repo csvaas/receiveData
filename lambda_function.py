5b83ac36ebeea307d1026e66c706b3289e540656
Dennis Bronnbauer <dennis-bronnbauer@gmx.de>
2022-02-12 09:07:27 +0100

    initial

---
+++ lambda_function.py
@@ -0,0 +1,9 @@
+import json
+import boto3
+
+
+def lambda_handler(event, context):
+    # return {"statusCode": 200, "body": "Hallo Dennis & Dennis"}
+    return {
+        'statusCode':   200
+    }
