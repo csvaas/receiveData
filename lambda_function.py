98d6db71d0d2e1cdd9289ffabe78a708f746fc12
Dennis Bronnbauer <dennis-bronnbauer@gmx.de>
2022-02-12 09:33:13 +0100

    Check Content-Type

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,16 @@
 import json
 import boto3
 
+CONTENT_TYPE = "Content-Type"
+CONTENT_TYPE_VAL = "application/json"
+
 
 def lambda_handler(event, context):
     client = boto3.client("lambda")
+    header = event["headers"]
 
-    return {"statusCode": 200}
+    # Check Content-Type
+    if header[CONTENT_TYPE] != CONTENT_TYPE_VAL:
+        return {"statusCode": 400, "text": json.loads(header)}
+
+    return {"statusCode": 200, "text": json.loads(header)}
